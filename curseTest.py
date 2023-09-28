107e45fd27689b3f1f68d9c8f69324bd6a1589e7
tarynnielsen <109117347+tarynnielsen@users.noreply.github.com>
2023-09-28 16:14:03 -0700

    Create curseTest.py

---
+++ curseTest.py
@@ -0,0 +1,26 @@
+import curses
+curses.initscr()
+curses.noecho()
+curses.cbreak() # don't have to press enter
+stdscr.keypad(True)
+
+try:
+    while True:
+        char = screen.getch()
+        if char == ord('q')
+            break
+        elif char == curses.KEY_UP:
+            print "up"
+        elif char == curses.KEY_DOWN:
+            print "down"
+        elif char == curses.KEY_RIGHT:
+            print "right"
+        elif char == curses.KEY_LEFT:
+            print "left"
+        elif char == 10:
+            print "stop"
+finally:
+    curse.nocbreak()
+    screen.keypad(0)
+    curses.echo()
+    curses.endwin()
