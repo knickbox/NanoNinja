009b677729e066fa57a29bfa23beb4c0410d9551
tarynnielsen <109117347+tarynnielsen@users.noreply.github.com>
2023-09-28 15:54:49 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,40 @@
+from keyboard_listener import KeyboardListener, Combo, KeyWord
+import sys
+
+def moveForward():
+	car.control_car(100,100)
+
+def moveLeft():
+	car.control_car(-100,100)
+
+def moveRight():
+	car.control_car(100,-100)
+
+def moveBackward():
+	car.control_car(-100,-100)
+
+def stop():
+	car.control_car(0,0)
+
+def exitMode():
+	sys.exit()
+
+combinations = {
+	'function 1': Combo(['alt'], 'f', moveLeft),
+	'function 2': Combo(['alt'], 'g', moveRight)
+}
+
+keywords = {
+	'keyword_1': KeyWord('w', moveForward),
+	'keyword_2': KeyWord('s', moveBackward),
+	'left': KeyWord('a', moveLeft),
+	'right': KeyWord('d', moveRight),
+	'exitWord': KeyWord('q', exitMode)
+	'stop': KeyWord(' ', stop)
+}
+
+keyboard_listener = KeyboardListener(combinations=combinations, keywords=keywords)
+keyboard_listener.run()
+
+
+
