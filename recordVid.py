83d5a17d79944b10bf0f229df8f9f560c6464ed3
tarynnielsen <109117347+tarynnielsen@users.noreply.github.com>
2023-10-17 16:16:52 -0700

    Update recordVid.py

--- recordVid.py
+++ recordVid.py
@@ -4,7 +4,7 @@ from time import sleep
 camera = PiCamera()
 
 camera.start_preview()
-camera.start_recording([file path])
+camera.start_recording('~/firstVideo.h264')
 sleep(10)
 camera.stop_recording()
 camera.stop_preview()
