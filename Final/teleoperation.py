1c2459d6b7944e0e40aeff694551ef9d74fe83de
knickbox <knicbox@gmail.com>
2023-12-01 14:16:00 -0700

    Copy teleoperation into Final

---
+++ Final/teleoperation.py
@@ -0,0 +1,33 @@
+import curses
+from Car import Car
+car = Car()
+screen = curses.initscr()
+curses.noecho()
+curses.cbreak() # don't have to press enter
+screen.keypad(True)
+
+try:
+    while True:
+        char = screen.getch()
+        if char == ord('q'):
+            break
+        elif char == curses.KEY_UP:
+            print("up")
+            car.control_car(100,100)
+        elif char == curses.KEY_DOWN:
+            print("down")
+            car.control_car(-100,-100)
+        elif char == curses.KEY_RIGHT:
+            print("right")
+            car.control_car(100,-100)
+        elif char == curses.KEY_LEFT:
+            print("left")
+            car.control_car(-100,100)
+        elif char == ord('m'):
+            print("stop")
+            car.control_car(0,0)
+finally:
+    curse.nocbreak()
+    screen.keypad(0)
+    curses.echo()
+    curses.endwin()
