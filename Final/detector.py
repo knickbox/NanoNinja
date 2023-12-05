32eff77e4f2968195401a64d936014cfa04b2b26
knickbox <knicbox@gmail.com>
2023-12-05 13:23:25 -0700

    trying to get most recent image

--- Final/detector.py
+++ Final/detector.py
@@ -4,11 +4,11 @@ from ultralytics import YOLO
 class Detector:
     def __init__(self):
         self.cam = cv2.VideoCapture(0)
+        self.cam.set(cv2.CAP_PROP_BUFFERSIZE, 0)
         self.model = YOLO('yolov8n.pt')  # load an official model
 
     def detect(self):
         result, image = self.cam.read()
-        image = cv2.imread('../../test.jpg')
         if True:
             image = cv2.resize(image, (320, 320))
             return self.model(image)  # predict on an image
