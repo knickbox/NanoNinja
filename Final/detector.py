0cdc62215da3cef0143770a15a7437539197d366
knickbox <knicbox@gmail.com>
2023-12-10 13:33:34 -0700

    buffer flush test

--- Final/detector.py
+++ Final/detector.py
@@ -4,14 +4,20 @@ from ultralytics import YOLO
 class Detector:
     def __init__(self):
         self.cam = cv2.VideoCapture(0)
-        self.cam.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+        # self.cam.set(cv2.CAP_PROP_BUFFERSIZE, 1)
         self.model = YOLO('yolov8n.pt')  # load an official model
 
     def detect(self):
-        result, image = self.cam.read()
-        if True:
+        result = False
+        image = None
+        # grab frames a bunch of times to clear the buffer (cv2 does not have a better way)
+        for i in range(5):
+            self.cam.grab()
+        if result:
             image = cv2.resize(image, (320, 320))
-            return self.model(image)  # predict on an image
+            # return self.model(image)  # predict on an image
+            # test section (delete later)
+            cv2.imwrite('out.jpg', image)
         else:
             return None
         
