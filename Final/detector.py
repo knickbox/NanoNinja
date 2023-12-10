8d3d0146d9d0243ca8abd0c1c2aad3fc68249484
knickbox <knicbox@gmail.com>
2023-12-10 14:28:55 -0700

    Detector.detect now returns count of people

--- Final/detector.py
+++ Final/detector.py
@@ -4,10 +4,12 @@ from ultralytics import YOLO
 class Detector:
     def __init__(self):
         self.cam = cv2.VideoCapture(0)
-        # self.cam.set(cv2.CAP_PROP_BUFFERSIZE, 1)
         self.model = YOLO('yolov8n.pt')  # load an official model
 
+    
     def detect(self):
+        """Takes an image and returns the number of people detected by the camera"""
+        
         result = False
         image = None
         # grab frames a bunch of times to clear the buffer (cv2 does not have a better way)
@@ -15,13 +17,18 @@ class Detector:
             self.cam.grab()
         result, image = self.cam.read()
         if result:
-            image = cv2.resize(image, (320, 320))
-            # return self.model(image)  # predict on an image
-            # test section (delete later)
-            results = self.model(image)
-            annotated_frame = results[0].plot()
-            cv2.imwrite('out.jpg',annotated_frame)
+            image = cv2.resize(image, (320, 320)) # not super necessary but might save memory
+            results = self.model(image, verbose=False) # predict on an image
+            
+            ## uncomment to save the image with the bounding boxes
+            # annotated_frame = results[0].plot()
+            # cv2.imwrite('out.jpg',annotated_frame)
 
+            names = self.model.names
+            # get the 'person' class id
+            person_id = list(names)[list(names.values()).index('person')]
+            # count 'person' objects in the results
+            personCount = results[0].boxes.cls.tolist().count(person_id)
+            return personCount
         else:
             return None
-        
