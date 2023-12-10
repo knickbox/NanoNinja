a647db136b0093a53a13c24611ab45ba82d68bd0
knickbox <knicbox@gmail.com>
2023-12-10 13:54:03 -0700

    cam test with yolo

--- Final/detector.py
+++ Final/detector.py
@@ -13,11 +13,15 @@ class Detector:
         # grab frames a bunch of times to clear the buffer (cv2 does not have a better way)
         for i in range(5):
             self.cam.grab()
+        result, image = self.cam.read()
         if result:
             image = cv2.resize(image, (320, 320))
             # return self.model(image)  # predict on an image
             # test section (delete later)
-            cv2.imwrite('out.jpg', image)
+            results = self.model(image)
+            annotated_frame = results[0].plot()
+            cv2.imwrite('out.jpg',annotated_frame)
+
         else:
             return None
         
